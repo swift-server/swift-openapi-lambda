0c14afac03743e55feda1cb3c382e1f643df6a5e
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2024-05-15 12:57:43 +0200

    restore original license header in files borrowed from the Swift OpenAPI Generator project

--- scripts/check-for-docc-warnings.sh
+++ scripts/check-for-docc-warnings.sh
@@ -14,6 +14,20 @@
 ##
 ##===----------------------------------------------------------------------===##
 
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the SwiftOpenAPIGenerator open source project
+##
+## Copyright (c) 2023 Apple Inc. and the SwiftOpenAPIGenerator project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of SwiftOpenAPIGenerator project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
 set -euo pipefail
 
 log() { printf -- "** %s\n" "$*" >&2; }

1ed71312ebf67b2bb9d8522031d44ba94a3dbc42
Victor Hugo <hey.zooi@gmail.com>
2025-06-13 12:47:23 -0300

    Upgrade swift-aws-lambda-events

--- Package@swift-5.10.swift
+++ Package@swift-5.10.swift
@@ -10,7 +10,7 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.0.0"),
         .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0-alpha.3"),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "0.3.0"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "1.0.0"),
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
     ],
     targets: [

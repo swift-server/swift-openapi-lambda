31f4fd900d17e872293c235098e4f7631bc9f814
Sebastien Stormacq <sebastien.stormacq@gmail.com>
2025-08-30 22:19:04 +0200

    swift license

--- scripts/check_format.sh
+++ scripts/check_format.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+##===----------------------------------------------------------------------===##
+##
+## This source file is part of the Swift OpenAPI Lambda open source project
+##
+## Copyright (c) 2023 Amazon.com, Inc. or its affiliates
+##                    and the Swift OpenAPI Lambda project authors
+## Licensed under Apache License v2.0
+##
+## See LICENSE.txt for license information
+## See CONTRIBUTORS.txt for the list of Swift OpenAPI Lambda project authors
+##
+## SPDX-License-Identifier: Apache-2.0
+##
+##===----------------------------------------------------------------------===##
+
 # ===----------------------------------------------------------------------===//
 #
 # This source file is part of the Swift.org open source project

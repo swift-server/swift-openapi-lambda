ba1e41a14d6c681d160a8c71cf99ada7b6d6b034
Sébastien Stormacq <sebastien.stormacq@gmail.com>
2025-09-02 19:49:18 +0200

    Update quote API example

--- Examples/quoteapi/Package.swift
+++ Examples/quoteapi/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,18 +6,18 @@ import PackageDescription
 let package = Package(
     name: "QuoteService",
     platforms: [
-        .macOS(.v13), .iOS(.v15), .tvOS(.v15), .watchOS(.v6),
+        .macOS(.v15)
     ],
     products: [
         .executable(name: "QuoteService", targets: ["QuoteService"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-openapi-generator.git", from: "1.4.0"),
-        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.5.0"),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "1.0.0-alpha.3"),
-        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "0.4.0"),
-        .package(url: "https://github.com/swift-server/swift-openapi-lambda.git", from: "0.2.0"),
-        // .package(name: "swift-openapi-lambda", path: "../swift-openapi-lambda")
+        .package(url: "https://github.com/apple/swift-openapi-runtime.git", from: "1.8.2"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-runtime.git", from: "2.0.0-beta.1"),
+        .package(url: "https://github.com/swift-server/swift-aws-lambda-events.git", from: "1.2.0"),
+        // .package(url: "https://github.com/swift-server/swift-openapi-lambda.git", from: "0.3.0"),
+        .package(name: "swift-openapi-lambda", path: "../.."),
     ],
     targets: [
         .executableTarget(
@@ -28,7 +28,7 @@ let package = Package(
                 .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
                 .product(name: "OpenAPILambda", package: "swift-openapi-lambda"),
             ],
-            path: "Sources",
+            path: "Sources/QuoteAPI",
             resources: [
                 .copy("openapi.yaml"),
                 .copy("openapi-generator-config.yaml"),
@@ -39,6 +39,14 @@ let package = Package(
                     package: "swift-openapi-generator"
                 )
             ]
-        )
+        ),
+        .executableTarget(
+            name: "LambdaAuthorizer",
+            dependencies: [
+                .product(name: "AWSLambdaRuntime", package: "swift-aws-lambda-runtime"),
+                .product(name: "AWSLambdaEvents", package: "swift-aws-lambda-events"),
+            ],
+            path: "Sources/LambdaAuthorizer"
+        ),
     ]
 )
--- Examples/quoteapi/Sources/QuoteAPI/QuoteService.swift
+++ Examples/quoteapi/Sources/QuoteAPI/QuoteService.swift
@@ -16,12 +16,39 @@
 import Foundation
 import OpenAPIRuntime
 import OpenAPILambda
+// import ServiceLifecycle
 
 @main
 struct QuoteServiceImpl: APIProtocol, OpenAPILambdaHttpApi {
 
     init(transport: OpenAPILambdaTransport) throws {
         try self.registerHandlers(on: transport)
+    }
+
+    static func main() async throws {
+
+        // when you just need to run the Lambda function, call Self.run()
+        try await Self.run()
+
+        // when you need to have access to the runtime for advanced usage (dependency injection, Service LifeCycle, etc)
+        // create the LambdaRuntime and run it
+
+        // Here is an example with Service Lifecycle
+        // Add the following in Package.swift
+        //    .package(url: "https://github.com/swift-server/swift-service-lifecycle.git", from: "2.6.0"),
+        // and
+        //    .product(name: "ServiceLifecycle", package: "swift-service-lifecycle"),
+        // Add `import ServiceLifecycle` at the top of this file`
+
+        // let lambdaRuntime = try LambdaRuntime(body: Self.handler())
+        // try await lambdaRuntime.run()
+        // let serviceGroup = ServiceGroup(
+        //     services: [lambdaRuntime],
+        //     gracefulShutdownSignals: [.sigterm],
+        //     cancellationSignals: [.sigint],
+        //     logger: Logger(label: "ServiceGroup")
+        // )
+        // try await serviceGroup.run()
     }
 
     func getQuote(_ input: Operations.getQuote.Input) async throws -> Operations.getQuote.Output {
